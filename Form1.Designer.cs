79cc7d11b1ddb1d0463e9a7dfd3a8b74fa5b2736
Gabriel <ST10085443@vcconnect.edu.za>
2022-08-09 22:13:37 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace prjProcessManipulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtProcess = new System.Windows.Forms.TextBox();
+            this.cmbOperation = new System.Windows.Forms.ComboBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.lbListProcess = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Process Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Operation";
+            // 
+            // txtProcess
+            // 
+            this.txtProcess.Location = new System.Drawing.Point(216, 130);
+            this.txtProcess.Name = "txtProcess";
+            this.txtProcess.Size = new System.Drawing.Size(121, 23);
+            this.txtProcess.TabIndex = 2;
+            // 
+            // cmbOperation
+            // 
+            this.cmbOperation.FormattingEnabled = true;
+            this.cmbOperation.Items.AddRange(new object[] {
+            "Start",
+            "Stop"});
+            this.cmbOperation.Location = new System.Drawing.Point(216, 189);
+            this.cmbOperation.Name = "cmbOperation";
+            this.cmbOperation.Size = new System.Drawing.Size(121, 23);
+            this.cmbOperation.TabIndex = 3;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(240, 238);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 4;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // lbListProcess
+            // 
+            this.lbListProcess.FormattingEnabled = true;
+            this.lbListProcess.ItemHeight = 15;
+            this.lbListProcess.Location = new System.Drawing.Point(420, 61);
+            this.lbListProcess.Name = "lbListProcess";
+            this.lbListProcess.Size = new System.Drawing.Size(353, 364);
+            this.lbListProcess.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbListProcess);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.cmbOperation);
+            this.Controls.Add(this.txtProcess);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtProcess;
+        private System.Windows.Forms.ComboBox cmbOperation;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.ListBox lbListProcess;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjProcessManipulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ListAllProcesses();
+
+        }
+
+        public void ListAllProcesses()
+        {
+            lbListProcess.Items.Clear();
+            Process[] ProcessesList = Process.GetProcesses();
+
+            foreach (var pr in ProcessesList)
+            {
+                lbListProcess.Items.Add(pr.ProcessName);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = txtProcess.Text;
+            if (cmbOperation.SelectedItem.ToString().Contains("Start"))
+            {
+                lbListProcess.Items.Add("Start was selected");
+                process.Start();
+            }
+            else if (cmbOperation.SelectedItem.ToString().Contains("Stop"))
+            {
+                foreach (var proc in Process.GetProcessesByName(txtProcess.Text))
+                {
+                    proc.Kill();
+                }
+            }
+            ListAllProcesses();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
